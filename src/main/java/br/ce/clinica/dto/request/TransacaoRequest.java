5e937902a62b8217fd41fd1065fa9bdb36b9dc2c
Haroldo <haroldomorais92@gmail.com>
2025-12-31 11:56:50 -0300

    Adiciona anotações OpenAPI ao modelo TransacaoRequest

--- src/main/java/br/ce/clinica/dto/request/TransacaoRequest.java
+++ src/main/java/br/ce/clinica/dto/request/TransacaoRequest.java
@@ -2,6 +2,7 @@ package br.ce.clinica.dto.request;
 
 import br.ce.clinica.enums.TipoDePagamento;
 import lombok.*;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @Getter
 @Setter
@@ -10,14 +11,19 @@ import lombok.*;
 @NoArgsConstructor
 public class TransacaoRequest {
 
+    @Schema(name = "caixaEntrada", description = "Valor de entrada no caixa", examples = { "1500" })
     private Integer caixaEntrada;
 
+    @Schema(name = "caixaSaida", description = "Valor de saída no caixa", examples = { "500" })
     private Integer caixaSaida;
 
+    @Schema(name = "descricao", description = "Descrição da transação", examples = { "Pagamento de consulta médica" })
     private String descricao;
 
+    @Schema(name = "tipoDePagamento", description = "Tipo de pagamento", examples = { "DINHEIRO" })
     private TipoDePagamento tipoDePagamento;
 
+    @Schema(name = "pacienteId", description = "Id do paciente", examples = { "1" })
     private Long pacienteId;
 
 }
